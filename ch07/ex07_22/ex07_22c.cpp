374adfc8e56dccd583774fd252f2f22a493896d4
lesvaughn <les.vaughn@sbcglobal.net>
2021-12-15 10:07:32 -0600

    Init

--- ch07/ex07_22/ex07_22c.cpp
+++ ch07/ex07_22/ex07_22c.cpp
@@ -19,10 +19,18 @@
 int main() {
    const unsigned int row{8};
    const unsigned int column{8};
-
    std::array<int, column> horizontal{2, 1, -1, -2, -2, -1, 1, 2};
    std::array<int, row> vertical{-1, -2, -2, -1, 1, 2, 2, 1};
-   std::array<std::array<int, column>, row> accessibility {2, 3, 4, 4, 4, 4, 3, 2,
+   std::default_random_engine moveEngine{static_cast<unsigned int>(time(0))};
+   std::uniform_int_distribution<size_t> randomMove{0, 7};
+   int totalFulltours{0};
+
+   for (size_t startRow{0}; startRow <= 7; ++startRow)
+   {
+      for (size_t startColumn{0}; startColumn <= 7; ++startColumn)
+      {
+         std::array<std::array<int, column>, row> board{0};
+         std::array<std::array<int, column>, row> accessibility {2, 3, 4, 4, 4, 4, 3, 2,
                                                            3, 4, 6, 6, 6, 6, 4, 3,
                                                            4, 6, 8, 8, 8, 8, 6, 4,
                                                            4, 6, 8, 8, 8, 8, 6, 4,
@@ -30,15 +38,6 @@ int main() {
                                                            4, 6, 8, 8, 8, 8, 6, 4,
                                                            3, 4, 6, 6, 6, 6, 4, 3,
                                                            2, 3, 4, 4, 4, 4, 3, 2};
-
-   std::default_random_engine moveEngine{static_cast<unsigned int>(time(0))};
-   std::uniform_int_distribution<size_t> randomMove{0, 7};
-
-   for (size_t startRow{0}; startRow <= 1; ++startRow)
-   {
-      for (size_t startColumn{0}; startColumn <= 0; ++startColumn)
-      {
-         std::array<std::array<int, column>, row> board{0};
          unsigned int counter{0};
          unsigned int moveCount{0};
          unsigned int currentRow = startRow;
@@ -79,7 +78,7 @@ int main() {
                currentRow = bestRow;
                currentColumn = bestColumn;
                ++moveCount;
-               board[currentRow][currentColumn] == moveCount;
+               board[currentRow][currentColumn] = moveCount;
          
                //std::cout << "Move " << moveCount << ":  Row - " << currentRow
                 //  << "   Column -  " << currentColumn << std::endl;
@@ -89,8 +88,14 @@ int main() {
 
          } // end while
           std::cout << " and took " << moveCount << " moves." << std::endl;
+          if (moveCount == 64)
+          {
+             ++totalFulltours;
+          }
       } // for j
      
    } // end for i
+
+   std::cout << "\nThere were " << totalFulltours << " full tours" << std::endl;
 
 }
